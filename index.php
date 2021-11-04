0e07c5dd3ea0c2c1b1a58ced983adc00e3783d2f
jweaver0312 <weaver.jarod@outlook.com>
2021-11-04 03:14:33 -0400

    Update index.php
    works without the body tags, was just in a HTML mode at the moment.

--- index.php
+++ index.php
@@ -4,9 +4,6 @@
     <link rel="stylesheet" href="style.css" type="text/css">
   </head>
   
-  <body>
-    <?php
-    	header("Refresh: 5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    ?>
-  </body>
+  <?php header("Refresh: 5; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ"); ?>
+  
 </html>
